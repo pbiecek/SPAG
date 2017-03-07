03024ecddca17e01a88d61b3d5b6adadbd3c889e
Maksymilian Mazur <mazurmaksymilian@gmail.com>
2017-03-07 06:24:41 +0100

    Dodaje działający plik i dane, paczka powinna dać się zainstalować, a korzystając z danych można już liczyć indeks. W kolejnym komicie dodam testy+liczenie indeksu z podziałek na składowe shapefile (wojewodztwa)

--- SPAG/R/SPAG.R
+++ SPAG/R/SPAG.R
@@ -2,36 +2,62 @@
 #'
 #' @param companiesDF - data frame with information regarding the companies. At least four columns are required:
 #' x and y coordinates of the company, the category of company and a numeric
+#' @param shp - SpatialPolygonsDataFrame object obtained via loading a shapefile
 #' @param xInd - number of the column in companiesDF with information regarding the latitude
 #' @param yInd - number of the column in companiesDF with information regarding the longitude
 #' @param empInd - number of the column in companiesDF with numeric data regarding the employment
 #' @param categInd - number of the column in companiesDF information about the category of the company
-#' @param shp - SpatialPolygonsDataFrame object obtained via loading a shapefile
 #'
+#' @examples 
+#' data(CompaniesPoland)
+#' data(ShapefilePoland)
+#' spagIndex <- SPAG(CompaniesPoland,ShapefilePoland)
+#' print(spagIndex)
+#' plot(spagIndex)
+#' plot(spagIndex, addCompanies=FALSE)
+#' plot(spagIndex, "A")
+#' plot(spagIndex, "B")
+#' plot(spagIndex, "C")
+#' 
 #' @export
 
+SPAG <- function(companiesDF, shp, xInd = 1, yInd=2, empInd = 3, categInd = 4){
 
+  currentWarning <- getOption("warn")
+  options(warn = -1)
+  
+  # Possibly fix this handle
+  # TODO: Which Transform to use?
+  if(is.projected(shp) %in% c(NA, FALSE)){
+    stop("The shapefile has no projection")
+  } else{
+    shp <-spTransform(shp, CRS("+proj=tmerc +lat_0=0 +lon_0=18.99999999999998 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +towgs84=0,0,0 +units=m +no_defs"))
+  }
 
-
-SPAG <- function(companiesDF, xInd = 1, yInd=2, empInd = 3, categInd = 4, shp){
-  # Calculating the distance part of SPAG index
-
+  # TODO: In general - add some conditions on the shapefile - Coordinate System etc.
+  # TODO: handle not empty shapefile 
   categories <- unique(companiesDF[,categInd])
   vectorOfEmployment <- companiesDF[,empInd]
   totalEmployment <- sum(vectorOfEmployment)
 
+  # Calculating the coverage part of SPAG index:
+  
   ICov <- sapply(categories,
                       function(x){
                         return (sum(companiesDF[companiesDF[,categInd]==x,empInd])/totalEmployment)
                       })
 
-  # calculating base radius for other indexes
+  # Calculating base radius for other indexes
+  # In order to do so I change the CRS to a system in which the area is not showed in degrees
+  # as circles in those coordinates are oblate ellipses.
+  
+  newCoordinateSystem<-"+init=epsg:3347"
+  # CRS in degrees
+  newCoordinateSystem<-"+proj=longlat +datum=WGS84"
+  
+  region<-spTransform(shp, CRS(newCoordinateSystem))
 
- # projekcja<-"+init=epsg:3347"
- # region<-spTransform(shp, CRS(projekcja))
-
-
-  area <- gArea(shp)
+  area <- gArea(region)
 
   rBase <- sapply(categories,
                       function(x){
@@ -50,22 +76,25 @@ SPAG <- function(companiesDF, xInd = 1, yInd=2, empInd = 3, categInd = 4, shp){
   radiusVectorTotal <-sqrt(companiesDF[,empInd])*rBaseTotal
 
   vectorOfRadius <- sqrt(companiesDF[,empInd])*baseRadiusVector
-  xySP <- SpatialPoints(companiesDF[,c(xInd,yInd)])
+  
+  # Currently I assume the points in the data frame are traditional coordinates:
+  xySP <- SpatialPoints(companiesDF[,c(xInd,yInd)], proj4string=CRS("+proj=longlat +datum=WGS84"))
+  # Transforming the coordinates to be in the same system as the shapefile
+  xySP2 <- spTransform(xySP, CRS(newCoordinateSystem))
+  
+  # New circles will appear as circluar in plot
+  circles <-gBuffer(xySP2, quadsegs=150, byid=TRUE, width=vectorOfRadius)
 
-  circles <-gBuffer(xySP, quadsegs=150, byid=TRUE, width=vectorOfRadius)
-  circlesTotal <- gBuffer(xySP, quadsegs=150, byid=TRUE, width=radiusVectorTotal)
-
-
-
-  projekcja<-"+proj=longlat +datum=WGS84"
-  region<-spTransform(shp, CRS(projekcja))
   # Coverage Index for all the companies
   IDist<- sapply(categories,
            function(x){
              theoreticalCompanies <- spsample(region, nrow(companiesDF[companiesDF[,categInd]==x,]), type="regular")
              theoreticalDF <- as.data.frame(theoreticalCompanies)
              theoreticalDist<-dist(as.matrix(theoreticalCompanies@coords))
-             mean(dist(as.matrix(companiesDF[companiesDF[,categInd]==x,c(xInd,yInd)])))/mean(theoreticalDist)
+             meanDist <- mean(dist(as.matrix(companiesDF[companiesDF[,categInd]==x,c(xInd,yInd)])))/mean(theoreticalDist)
+             if (is.finite(meanDist)){
+               return(meanDist)
+             } else return(0)
            })
 
   theoreticalCompanies <- spsample(region, nrow(companiesDF), type="regular")
@@ -86,8 +115,12 @@ SPAG <- function(companiesDF, xInd = 1, yInd=2, empInd = 3, categInd = 4, shp){
                           unionArea <- gUnaryUnion(circles[companiesDF[,categInd]==x])
                           unionArea
                         })
+  
+  CategoryArea$proj4string = CRS("+proj=longlat +datum=WGS84")
 
   # wont work when name total is in a category
+  # redundant code - clean it up
+  
   names(CategoryArea) <- categories
   CategoryArea$total <- gUnaryUnion(circles)
 
@@ -102,30 +135,39 @@ SPAG <- function(companiesDF, xInd = 1, yInd=2, empInd = 3, categInd = 4, shp){
 
   names(IndexTotal) <- c("categories","IDist","IOver","ICov","ISPAG")
   IndexDF <- rbind(IndexDF,IndexTotal)
+  
+  companyList <- list(companies = CompaniesPoland, xInd = xInd, yInd=yInd, empInd = empInd, categInd = categInd)
 
-
-  x <- list( map = shp, unionAreaList = CategoryArea, SPAGIndex = IndexDF)
+  x <- list( map = region , unionAreaList = CategoryArea, companiesList = companyList, SPAGIndex = IndexDF)
   class(x) <- "SPAG"
 
-
+  options(warn = currentWarning)
   return(x);
 }
 
 
+plot.SPAG = function(x, category="total", addCompanies=TRUE){
 
+  mapDF <- fortify(x$map)
+  unionArea <- fortify(x$unionAreaList[[category]])
+  
+  if(category=="total"){
+    companies <- x$companiesList$companies
+  } else {
+    companies <- x$companiesList$companies[x$companiesList$companies[,x$companiesList$categInd]==category,]
+  }
+  
+  mapPlot <- ggplot() +
+             geom_polygon(data=unionArea, aes(long, lat, group=group), colour='red', fill=NA) +
+             geom_polygon(data=mapDF, aes(long, lat, group=group), colour='#808080', fill=NA) +
+             theme_bw() +
+             labs(x="longitude", y="latitude")
+  
+if(addCompanies){mapPlot <- mapPlot + geom_point(data=companies[,c(x$companiesList$xInd,x$companiesList$yInd)], aes(x,y),size=0.4)}
+  mapPlot
+  
+}
 
-plot.SPAG <- function(obj, category){
-
-  frameCircle <- obj$unionAreaList[[category]]@bbox
-  frameMap <- obj$map@bbox
-
-  frameFinal <- matrix(c(
-    min(frameMap[1,1],frameCircle[1,1]),
-    max(frameMap[1,2],frameCircle[1,2]),
-    min(frameMap[2,1],frameCircle[2,1]),
-    max(frameMap[2,2],frameCircle[2,2])
-  ), nrow=2, byrow=TRUE)
-
-  plot(obj$map, xlim=c(frameFinal[1,1], frameFinal[1,2]), ylim=c(frameFinal[2,1], frameFinal[2,2]))
-  plot(obj$unionAreaList[[category]], border="red",add=TRUE)
+print.SPAG = function(x){
+  print(x$SPAGIndex)
 }
