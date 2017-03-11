bebf1db35721777cdfb31408297aea43b705bdf5
Maksymilian Mazur <mazurmaksymilian@gmail.com>
2017-03-11 02:38:34 +0100

    Poprawa zbiorów danych.

---
+++ R/CompaniesPoland.R
@@ -0,0 +1,15 @@
+#' Exemplary data for calculating an Index of Spatial agglomeration
+#'
+#' This data frame contains a random set of companies located across Poland. Every company has an artificial
+#' category assigned, which defines the field of work the company focuses on and the number of employees working
+#' there.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name CompaniesPoland
+#' @usage CompaniesPoland
+#' @format CompaniesPoland is a data frame with 13 rows (companies) and 4 columns (company parameters) named lon and lat 
+#' (geographic longitude and latitude of the company), employment and category.
+#'
+#'
+"CompaniesPoland"
---
+++ R/ShapefilePoland.R
@@ -0,0 +1,15 @@
+#' Exemplary map for calculating an Index of Spatial agglomeration
+#'
+#' This object is a SpatialPolygonsDataFrame obtained via using readOGR function from rgdal package on a shapefile.
+#' The data frame represents a map of Poland divided into voivodeships.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name ShapefilePoland
+#' @usage ShapefilePoland
+#' 
+#' @format ShapefilePoland is a SpatialPolygonsDataFrame object representing a map of Poland.
+#'
+#' @seealso \code{\link{SpatialPolygonsDataFrame}}
+#'
+"ShapefilePoland"
