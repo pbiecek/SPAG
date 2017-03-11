5b88f4dc9d328be0e80329adf3077616e14e58c5
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-03-12 00:17:40 +0100

    Usunięte polskie znaki, usunięte duplikaty zależności , dodana zależnośc od R, korekta plików z dokumentacją

--- R/CompaniesPoland.R
+++ R/CompaniesPoland.R
@@ -3,13 +3,11 @@
 #' This data frame contains a random set of companies located across Poland. Every company has an artificial
 #' category assigned, which defines the field of work the company focuses on and the number of employees working
 #' there.
-#' 
+#'
 #' @docType data
 #' @keywords datasets
 #' @name CompaniesPoland
 #' @usage CompaniesPoland
 #' @format \code{CompaniesPoland} is a data frame with 13 rows (companies) and 4 columns (company parameters) named \code{long}
 #'  and \code{lat} (geographic longitude and latitude of the company), \code{emp} (employment) and \code{category}.
-#'
-#'
-"CompaniesPoland"
+NULL
