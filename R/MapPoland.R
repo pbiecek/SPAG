e25c8f4140778c28d8ba061c5c6a615273c85211
Maksymilian Mazur <mazurmaksymilian@gmail.com>
2017-08-02 00:45:21 +0200

    cleaning Data Sets

---
+++ R/MapPoland.R
@@ -0,0 +1,16 @@
+#' Exemplary map for calculating an Index of Spatial agglomeration
+#'
+#' This object is a \code{SpatialPolygonsDataFrame} obtained via using \code{readOGR} function from rgdal package on a shapefile.
+#' The data frame represents a map of Poland divided into voivodeships.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ShapefilePoland
+#' @usage ShapefilePoland
+#'
+#' @format \code{ShapefilePoland} is a \code{SpatialPolygonsDataFrame} object representing a map of Poland.
+#'
+#' @source \href{http://www.codgik.gov.pl/}{\code{Centralny Osrodek Dokumentacji Geodezyjnej i Kartograficznej}}
+#' @seealso \code{\link{SpatialPolygonsDataFrame}}
+#'
+NULL
